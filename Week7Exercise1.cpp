ca0a50a6de7b482e8b8f4b9e465984e652aac3a1
chemlzh <chemlzh@pku.edu.cn>
2020-04-01 16:55:47 +0800

    Update.

---
+++ Week7Exercise1.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+// #include <map>
+#include <unordered_map>
+using namespace std;
+unordered_map<string, string> dict;
+char meaning[15], words[15], tmp[15], c;
+long flag = 0, len = 0;
+int main() {
+	for (c = getchar(); (c != '\n') || (flag); c = getchar()) {
+		if (c == ' ') { flag = 1, len = 0; continue; }
+		else if (c == '\n') { 
+			flag = 0, len = 0; 
+			dict[string(words)] = string(meaning);
+			memset(words, 0, sizeof(words));
+			memset(meaning, 0, sizeof(meaning));
+			continue;
+		}
+		(!flag) ? (meaning[len++] = c) : (words[len++] = c);
+	}
+	while (scanf("%s", tmp) != EOF) {
+		if (dict.count(string(tmp))) printf("%s\n", dict[string(tmp)].c_str());
+		else printf("%s\n", "eh");
+	}	
+}
